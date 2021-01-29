86060d88eca669c514ea74c1820bd24ce81c2776
Carlos <crodas035@gmail.com>
2021-01-29 12:35:37 +0000

    Primer commit

---
+++ loginRegister/ForgotViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ForgotViewController.swift
+//  loginRegister
+//
+//  Created by user177278 on 1/26/21.
+//
+
+import UIKit
+
+class ForgotViewController: UIViewController {
+
+    
+    @IBOutlet weak var animationView : UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setUpAnimation(){
+       
+    }
+
+}
---
+++ loginRegister/loginRegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  loginRegisterViewController.swift
+//  loginRegister
+//
+//  Created by user177278 on 1/20/21.
+//
+
+import UIKit
+
+class loginRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
